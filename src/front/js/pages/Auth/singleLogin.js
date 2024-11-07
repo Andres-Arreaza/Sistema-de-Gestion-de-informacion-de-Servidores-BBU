a5c7bb11468d453ad5265632bbba033465f2824b
Alejandro García Navia <alejogarcia@unicauca.edu.co>
2024-11-07 00:56:30 +0000

    Project under construction

--- src/front/js/pages/Auth/singleLogin.js
+++ src/front/js/pages/Auth/singleLogin.js
@@ -44,12 +44,12 @@ export const SingleLogin = () => {
                 <div className="col bg-light">
                     <form>
                         <div className="mb-3">
-                            <label for="exampleInputEmail1" className="form-label">Email address</label>
+                            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
                             <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                             <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                         </div>
                         <div className="mb-3">
-                            <label for="exampleInputPassword1" className="form-label">Password</label>
+                            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
                             <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" className="form-control" id="exampleInputPassword1" />
                         </div>
 
