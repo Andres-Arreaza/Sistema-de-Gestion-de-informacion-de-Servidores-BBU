954e39558bedbcc762a99b5192eb7d2646d5d707
Alejandro García Navia <alejogarcia@unicauca.edu.co>
2024-12-04 02:24:23 +0000

    Eliminación de código comentado vista CreateTestimony

--- src/front/js/pages/CreateTestimony.js
+++ src/front/js/pages/CreateTestimony.js
@@ -4,18 +4,13 @@ import { Navigate, useNavigate } from "react-router-dom";
 
 const CreateTestimony = () => {
     const { store, actions } = useContext(Context)
-    // const [name, setName] = useState("")
     const [testimony, setTestimony] = useState("")
-    // const [photo, setPhoto] = useState(null)
     const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-        // console.log(photo, name, testimony)
         if (testimony) {
             let data = {
-                // name: name,
-                // photo: photo,
                 content: testimony
             }
             let resp = await actions.createTestimony(data)
@@ -28,25 +23,12 @@ const CreateTestimony = () => {
         }
     }
 
-    // const handleFileChange = (e) => {
-    //     setPhoto(e.target.files[0]);
-    // };
-
     return (
         <div className="vh-100 d-flex justify-content-center text-center align-items-center mb-5" style={{ background: "linear-gradient(135deg, #f5f7fa 20%, #c3cfe2 50%)" }}>
             <div className="mb-5">
                 <form onSubmit={handleSubmit} className="bg-white text-center mb-5 p-3" style={{ borderRadius: "15px", border: "3px solid green" }}>
                     <div className="d-flex flex-column justify-content-center text-center">
                         <h1 className="text-success pb-4"><b>PERSONAL TESTIMONY</b></h1>
-                        {/* <div className="col-12">
-                            <label htmlFor="exampleInputEmail1" className="form-label"><b>UPLOAD PHOTO</b></label>
-                             <input value={photo} onChange={(e) => setPhoto(e.target.value)} type="file" className="form-control my-2" photo="Photo" placeholder="Upload Photo" required /> 
-                            <input value={photo ? photo.name : ""} onChange={handleFileChange} type="file" className="form-control my-2" required />
-                        </div>
-                        <div className="col-12 mt-3">
-                            <label htmlFor="exampleInputEmail1" className="form-label"><b>NAME</b></label>
-                            <input value={name} onChange={(e) => setName(e.target.value)} type="text" className="form-control my-2" name="name" placeholder="name" required />
-                        </div> */}
                         <div className="col-12 mt-3">
                             <label htmlFor="exampleInputEmail1" className="form-label"><b>TESTIMONY</b></label>
                             <input value={testimony} onChange={(e) => setTestimony(e.target.value)} type="text" className="form-control my-2" name="Testimony" placeholder="Testimony" required />
